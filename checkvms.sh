df60e26e67db7f063da7b58d17f00bb2f6a7670d
Bit Second <bitsecondal@gmail.com>
2017-05-09 22:48:00 -0500

    Merged linuxmonitoring and vboxstart repositories

---
+++ checkvms.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# enable debugging
+# set -x
+
+function log_message {
+## LOG MESSAGES FROM THE SCRIPT
+
+	# echo $(date)" | "$*
+	echo $*
+}
+
+function get_running_vms {
+## GET THE LIST OF RUNNING VMS
+
+	log_message "Getting the list of VMs that are running"
+
+	${VBOXBIN} list runningvms | awk '{print $1}' > ${VMSRUNNING}
+
+	log_message "Done getting the list of VMs that are running"
+}
+
+function get_all_vms {
+## GET THE LIST OF ALL THE VMS
+
+	log_message "Getting the list of VMs that are created"
+
+	${VBOXBIN} list vms | awk '{print $1}' > ${VMSALL}
+
+	log_message "Done getting the list of VMs that are created"
+}
+
+function start_vms_notrunning {
+## COMPARES WHAT IS RUNNING TO WHAT IS NOT RUNNING AND STARTS THOSE THAT ARE NOT RUNNING
+	LINECOUNT=0
+
+	log_message "Comparing the running VMs to all VMs"
+
+	diff ${VMSRUNNING} ${VMSALL} > ${VMSDIFF}
+
+	log_message "Done comparing the running VMs to all VMs"
+
+	while read line
+	do
+		if [ ${LINECOUNT} -gt 0 ]; then
+		# echo ${line}
+			MACHINENAME=$(echo ${line} | awk '{print $2}' | sed 's/"//g')
+
+			log_message "Virtual machine ${MACHINENAME} is not running"
+
+			${VBOXBIN} startvm ${MACHINENAME} --type headless
+
+			log_message "Virtual machine ${MACHINENAME} has been requested to start"
+
+			log_message "Waiting..."
+
+			sleep 5
+
+			# CHECK TO SEE IF THE MACHINE DID START
+			ps -ef | grep virtualbox | grep ${MACHINENAME}
+
+			if [ $? -eq 0 ]; then
+				log_message "Virtual machine ${MACHINENAME} is running"
+			else
+				log_message "Virtual machine ${MACHINENAME} failed to start"
+			fi
+		fi
+
+		# INCREMENT COUNTER
+		LINECOUNT=$((${LINECOUNT} + 1))
+	done < ${VMSDIFF}
+}
+
+function cleanup {
+## CLEANUP AFTER RUNNING THE SCRIPT
+
+	log_message "Performing cleanup."
+
+	rm ${VMSRUNNING} ${VMSALL} ${VMSDIFF}
+
+	log_message "Done performing cleanup."
+}
+
+function main {
+	get_running_vms
+
+	get_all_vms
+
+	start_vms_notrunning
+
+	# cleanup
+}
+
+
+# Default Variables
+VBOXBIN=/usr/bin/VBoxManage
+VMSRUNNING=/var/tmp/vmsrunning.txt
+VMSALL=/var/tmp/vmsall.txt
+VMSDIFF=/var/tmp/vmsdiff.txt
+
+# LOAD CONFIGURATION FILE
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}
+. config.sh
+
+main $*
---
+++ ssh_test.sh
@@ -0,0 +1,57 @@
+#/bin/bash
+
+################################################################################
+# AUTHOR: Kenny Robinson, Bit Second (bitsecondal@gmail.com)
+# COPYRIGHT: Bit Second Tech 2016
+# WEB: www.bitsecondtech.com
+# DATE CREATED: 2016-05-09
+# USAGE: ssh_test.sh <user@hostname>
+# DESCRIPTION: Check to see if servers are online and listening on SSH. If the
+# server is not listening, then system will generate ticket and send email
+# notification to those defined in the bashrc configuration.
+# VERSION HISTORY
+# 2016/02/06 - Initial version.
+# 2016/05/09 - Added script to linuxmonitoring repo. Updated usage information
+# in the header. 
+################################################################################
+
+# Log the date
+date
+
+## SCRIPT MAIN ##
+
+toEmail=""
+ticketAPI=""
+
+if [ -z "$1" ]; then
+	# Display the help information if the host name is not provided. 
+	echo "ERROR: Login name not provided."
+	echo ""
+	echo "Usage: ssh_test.sh <user@hostname>"
+else
+	# ssh to the server
+
+	loginId=${1}
+
+	echo "Attempting to connect to ${loginId}"
+
+	ssh -q ${loginId} exit
+	if [ $? -ne 0 ]
+	then
+		echo "Unable to connect via SSH as ${1}. Sending notifications."
+
+		# Send email notification if the server down is internal.
+		mailx -s "Could not connect to ${loginId}" -t "${toEmail}" < /dev/null			
+			
+		# Create ticket for ticketing system.
+		${ticketAPI}
+
+	else
+		echo "Connection test successful for ${loginId}"
+	fi
+
+fi
+# end if string not empty
+
+# Log script end time.
+date
