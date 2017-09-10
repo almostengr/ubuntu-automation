31cba84033b15dcbbadf99dae9ae87718894e2e3
Bit Second <bitsecondal@gmail.com>
2017-09-10 17:45:34 -0500

    Added comment outputs before each command.

--- utils/auto_update.sh
+++ utils/auto_update.sh
@@ -15,18 +15,32 @@
 
 # MAKE SURE THAT THE ROOT USER IS RUNNING THE SCRIPT.
 if [ "$(id -u)" == "0" ]; then
+	/bin/date
+	
+	echo "Removing old software"
+
 	/usr/bin/apt-get autoremove --purge -y
+
+	echo "Cleaning packages"
 
 	/usr/bin/apt-get clean
 
+	echo "Updating repositories"
+
 	/usr/bin/apt-get update
+
+	echo "Installing updates"
 
 	/usr/bin/apt-get upgrade -y
 
 	# IF REBOOT FILE HAS BEEN CREATED, THEN DO REBOOT
 	if [ -f /var/run/reboot-required ]; then
+		echo "Reboot is required. Rebooting"
+
 		/sbin/reboot
 	fi
+
+	/bin/date
 else
 	# THROW ERROR IF NOT RUNNING AS ROOT
 	/bin/echo "ERROR: Must be root to run script."
