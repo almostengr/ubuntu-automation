a91bcad12934d52631919f04d3e13da1b044a6e8
Almost Engineer <bitsecondal@gmail.com>
2017-12-13 20:07:20 -0600

    Added comment block. Removed old comments

--- virtualbox/checkvms.sh
+++ virtualbox/checkvms.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# enable debugging
-# set -x
+####################################################################################
+# DESCRIPTION: Check to see if any virtual machines are not running. If they are
+# not running then the scripts are started. 
+# AUTHOR: Kenny Robinson, @almostengr
+####################################################################################
 
 function log_message {
 ## LOG MESSAGES FROM THE SCRIPT
-
-	# echo $(date)" | "$*
-	echo $*
+	echo $(date)" | "$*
 }
 
 function get_running_vms {
 ## GET THE LIST OF RUNNING VMS
 
 	log_message "Getting the list of VMs that are running"
-
 	${VBOXBIN} list runningvms | awk '{print $1}' > ${VMSRUNNING}
-
 	log_message "Done getting the list of VMs that are running"
 }
 
@@ -24,36 +23,26 @@ function get_all_vms {
 ## GET THE LIST OF ALL THE VMS
 
 	log_message "Getting the list of VMs that are created"
-
 	${VBOXBIN} list vms | awk '{print $1}' > ${VMSALL}
-
 	log_message "Done getting the list of VMs that are created"
 }
 
 function start_vms_notrunning {
 ## COMPARES WHAT IS RUNNING TO WHAT IS NOT RUNNING AND STARTS THOSE THAT ARE NOT RUNNING
 	LINECOUNT=0
-
+	
 	log_message "Comparing the running VMs to all VMs"
-
 	diff ${VMSRUNNING} ${VMSALL} > ${VMSDIFF}
-
 	log_message "Done comparing the running VMs to all VMs"
 
 	while read line
 	do
 		if [ ${LINECOUNT} -gt 0 ]; then
-		# echo ${line}
 			MACHINENAME=$(echo ${line} | awk '{print $2}' | sed 's/"//g')
-
 			log_message "Virtual machine ${MACHINENAME} is not running"
-
 			${VBOXBIN} startvm ${MACHINENAME} --type headless
-
 			log_message "Virtual machine ${MACHINENAME} has been requested to start"
-
 			log_message "Waiting..."
-
 			sleep 5
 
 			# CHECK TO SEE IF THE MACHINE DID START
@@ -75,22 +64,15 @@ function cleanup {
 ## CLEANUP AFTER RUNNING THE SCRIPT
 
 	log_message "Performing cleanup."
-
 	rm ${VMSRUNNING} ${VMSALL} ${VMSDIFF}
-
 	log_message "Done performing cleanup."
 }
 
 function main {
 	get_running_vms
-
 	get_all_vms
-
 	start_vms_notrunning
-
-	# cleanup
 }
-
 
 # Default Variables
 VBOXBIN=/usr/bin/VBoxManage
@@ -105,3 +87,4 @@ cd ${DIR}
 . config.sh
 
 main $*
+
