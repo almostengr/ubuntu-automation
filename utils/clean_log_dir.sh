56cef09d3b105142febfcc12994ca26650fea1df
Almost Engineer <bitsecondal@gmail.com>
2017-12-16 23:17:50 -0600

    Cleaned code. Updated header block

--- utils/clean_log_dir.sh
+++ utils/clean_log_dir.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 ################################################################################
-# AUTHOR: Kenny Robinson, Bit Second (bitsecondal@gmail.com)
-# COPYRIGHT: Bit Second Tech 2016
-# WEB: www.bitsecondtech.com
+# AUTHOR: Kenny Robinson, @almostengr
 # DATE CREATED: 2016-05-29
 # USAGE: clean_log_dir.sh
 # DESCRIPTION: Cleans the log directory on the machine. Log and compressed log 
@@ -15,25 +13,21 @@
 # FILES OLDER THAN THIS NUMBER OF DAYS WILL BE REMOVED 
 DELAY=30
 
-function perform_cleanup() {
-        # display the disk usage before cleanup
-        /bin/df -h .
-
-        # list all the files before removing
-        /usr/bin/find /var/log/* -type f -mtime +${DELAY} -exec ls -la {} \;
-
-        # remove the files
-        /usr/bin/find /var/log/* -type f -mtime +${DELAY} -exec rm {} \;
-
-        # display the disk usage after cleanup
-	/bin/df -h . 
-}
-
 function main {
 	/bin/date
 
 	if [ "$(id -u)" == "0" ]; then
-		perform_cleanup
+		# display the disk usage before cleanup
+		/bin/df -h .
+
+		# list all the files before removing
+		/usr/bin/find /var/log/* -type f -mtime +${DELAY} -exec ls -la {} \;
+
+		# remove the files
+		/usr/bin/find /var/log/* -type f -mtime +${DELAY} -exec rm {} \;
+
+		# display the disk usage after cleanup
+		/bin/df -h .
 	else
 		echo "ERROR: Must be root to run script."
 	fi
