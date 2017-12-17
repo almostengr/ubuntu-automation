74bd0d17143988b0a1b7dd479ca752e518f8e021
Almost Engineer <bitsecondal@gmail.com>
2017-12-16 23:16:05 -0600

    Updated header and removed whitespace

--- utils/auto_update.sh
+++ utils/auto_update.sh
@@ -2,15 +2,9 @@
 
 ################################################################################
 # Script: auto_update.sh
-# Description: Script to automatically perform
-# functions on ubuntu based servers.
-# Author: Kenny Robinson, Bit Second
+# Description: Script to automatically perform function on ubuntu servers.
+# Author: Kenny Robinson, @almostengr
 # Date: 2016-02-28
-#
-# Version History
-# 2016-02-28 - Initial version.
-# 2017-05-18 - Updated to use full file path commands intead of only script names.
-# 2017-06-03 - Updated if condition to see if script is being ran as root.
 ################################################################################
 
 # MAKE SURE THAT THE ROOT USER IS RUNNING THE SCRIPT.
@@ -18,19 +12,15 @@ if [ "$(id -u)" == "0" ]; then
 	/bin/date
 	
 	echo "Removing old software"
-
 	/usr/bin/apt-get autoremove --purge -y
 
 	echo "Cleaning packages"
-
 	/usr/bin/apt-get clean
 
 	echo "Updating repositories"
-
 	/usr/bin/apt-get update
 
 	echo "Installing updates"
-
 	/usr/bin/apt-get upgrade -y
 
 	# IF REBOOT FILE HAS BEEN CREATED, THEN DO REBOOT
