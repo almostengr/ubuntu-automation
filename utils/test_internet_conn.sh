a266783cfe35bd60e8ed375bca569cb044370b58
Almost Engineer <bitsecondal@gmail.com>
2017-12-16 23:23:00 -0600

    Cleaned up header block

--- utils/test_internet_conn.sh
+++ utils/test_internet_conn.sh
@@ -4,10 +4,7 @@
 # Description: Tests the connection speed of the internet by downloading a 10 MB
 # file from the internet.
 # Date: 2015-10
-# Author: Kenny Robinson
-#
-# Version History
-# 2016-02-05 - Added header block to script.
+# Author: Kenny Robinson, @almostengr
 ################################################################################
 
 ## SCRIPT MAIN ##
