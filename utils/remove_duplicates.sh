70d75b83f22e9ee5e73bb6adad6d6f48e7fd502d
almostengr <bitsecondal@gmail.com>
2018-06-01 22:04:11 -0500

    Added comments to actions being peformed

--- utils/remove_duplicates.sh
+++ utils/remove_duplicates.sh
@@ -3,17 +3,25 @@
 ###############################################################################
 # Script: remove_duplicates.sh
 # Author: Kenny Robinson, @almostengr
-# Description: Remove duplicate files from a particular directory. Need to be 
+# Description: Remove duplicate files from a particular directory. Need to be
 # in the diretory that you wish to clean BEFORE running this script.
 ###############################################################################
 
 date
 
+echo "Checking disk space"
+
 # check disk space before cleanup
 du -sh "$(pwd)"
 
+echo "Done checking disk space"
+echo "Removing duplicates"
+
 # perform cleanup
 fdupes -r -d -N "$(pwd)"
+
+echo "Done removing duplicates"
+echo "Removing empty directories"
 
 # remove empty directories
 EMPTYDIR=1
@@ -24,9 +32,15 @@ while [ ${EMPTYDIR} -gt 0 ]; do
 	
 	# check if new empty directories exist
 	EMPTYDIR=$(find . -type d -empty | wc -l)
+	echo "${EMPTYDIR} empty directories found"
 done
+
+echo "Done removing empty directories"
+echo "Checking disk space"
 
 # check disk space after cleanup
 du -sh "$(pwd)"
+
+echo "Done checking disk space"
 
 date
