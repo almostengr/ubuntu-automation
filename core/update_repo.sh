eb4488d5ef257500afdeaaf42bfe05eb62cfaccb
Bit Second <bitsecondal@gmail.com>
2018-06-05 21:18:55 -0500

    Removed date. Added fetch. Added comments

--- core/update_repo.sh
+++ core/update_repo.sh
@@ -3,15 +3,16 @@
 #################################################################################
 # Script: update_repo.sh
 # Description: Script to automatically update the code from the repository.
-# Author: Kenny Robinson, Bit Second (www.bitsecondtech.com)
+# Author: Kenny Robinson, @almostengr
 # Date: 2017-09-10
 #################################################################################
 
-/bin/date
+# pull the latest code from origin
+/usr/bin/git fetch --all
 
+# make sure on the master branch
 /usr/bin/git checkout master 
 
+# pull the latest version into the current version
 /usr/bin/git pull origin master
-
-/bin/date
 
