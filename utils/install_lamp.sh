d408fd881e5b7f3f4e2d83dc1ddb54be87c6b0ba
Bit Second <bitsecondal@gmail.com>
2018-06-05 20:56:33 -0500

    Added date to echo. Full path to echo.

--- utils/install_lamp.sh
+++ utils/install_lamp.sh
@@ -8,26 +8,24 @@
 
 # MAKE SURE THAT THE ROOT USER IS RUNNING THE SCRIPT.
 if [ "$(id -u)" == "0" ]; then
-	/bin/date
-
-	echo "Updating repositories"
+	/bin/echo "$(date) Updating repositories"
 	/usr/bin/apt-get update
 
-	echo "Installing packages"
+	/bin/echo "$(date) Installing packages"
 	/usr/bin/apt-get install -y apache2 mysql-server php5-mysql libapache2-mod-php5 php5-mcrypt
-
+	
 	# restart apache after install 
+	/bin/echo "$(date) Restarting apache"
 	/usr/sbin/service apache2 restart
 
 	# IF REBOOT FILE HAS BEEN CREATED, THEN DO REBOOT
 	if [ -f /var/run/reboot-required ]; then
-		echo "Reboot is required. Rebooting"
+		/bin/echo "$(date) Reboot is required. Rebooting"
+		/bin/sleep 5
 		/sbin/reboot
 	fi
-
-	/bin/date
 else
 	# THROW ERROR IF NOT RUNNING AS ROOT
-	/bin/echo "ERROR: Must be root to run script."
+	/bin/echo "$(date) ERROR: Must be root to run script."
 fi
 
