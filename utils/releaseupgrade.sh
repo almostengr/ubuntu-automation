7c6fc7a72629bfb4934231f38020b4d1c0c2428f
Bit Second <bitsecondal@gmail.com>
2018-06-05 21:00:38 -0500

    Removed date from being output

--- utils/releaseupgrade.sh
+++ utils/releaseupgrade.sh
@@ -8,8 +8,6 @@
 
 # MAKE SURE THAT THE ROOT USER IS RUNNING THE SCRIPT.
 if [ "$(id -u)" == "0" ]; then
-	/bin/date
-	
 	echo "Cleaning packages"
 	/usr/bin/apt-get clean
 
@@ -26,8 +24,6 @@ if [ "$(id -u)" == "0" ]; then
 		echo "Reboot is required. Rebooting"
 		/sbin/reboot
 	fi
-
-	/bin/date
 else
 	# THROW ERROR IF NOT RUNNING AS ROOT
 	/bin/echo "ERROR: Must be root to run script."
