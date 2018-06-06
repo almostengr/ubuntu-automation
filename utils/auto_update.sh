0ee63de2668cb9efbd72270fe7274d8da71eb572
Bit Second <bitsecondal@gmail.com>
2018-06-05 21:07:37 -0500

    Removed date from start and end. Added date to echo

--- utils/auto_update.sh
+++ utils/auto_update.sh
@@ -9,30 +9,26 @@
 
 # MAKE SURE THAT THE ROOT USER IS RUNNING THE SCRIPT.
 if [ "$(id -u)" == "0" ]; then
-	/bin/date
-	
-	echo "Removing old software"
+	echo "$(date) Removing old software"
 	/usr/bin/apt-get autoremove --purge -y
 
-	echo "Cleaning packages"
+	echo "$(date) Cleaning packages"
 	/usr/bin/apt-get clean
 
-	echo "Updating repositories"
+	echo "$(date) Updating repositories"
 	/usr/bin/apt-get update
 
-	echo "Installing updates"
+	echo "$(date) Installing updates"
 	/usr/bin/apt-get upgrade -y
 
 	# IF REBOOT FILE HAS BEEN CREATED, THEN DO REBOOT
 	if [ -f /var/run/reboot-required ]; then
-		echo "Reboot is required. Rebooting"
-
+		echo "$(date) Reboot is required. Rebooting"
+		sleep 5
 		/sbin/reboot
 	fi
-
-	/bin/date
 else
 	# THROW ERROR IF NOT RUNNING AS ROOT
-	/bin/echo "ERROR: Must be root to run script."
+	/bin/echo "$(date) ERROR: Must be root to run script."
 fi
 
