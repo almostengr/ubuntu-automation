ee971767032b239fae7394f7d599a682fdcce000
Almost Engineer <bitsecondal@gmail.com>
2018-06-08 17:23:32 -0500

    #5 Added example configuration file

---
+++ config.example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# send email if failures occur. Set to 1 for yes, 0 for no
+SENDMAIL=1
+
+# email address that notification should be sent to
+EMAILTO="test@example.com"
+
+# path to mailx or other email program
+MAILXBIN=/usr/bin/mailx
+
