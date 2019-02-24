2263823fa5a4e32cd98cf5960325b32845a58075
Bit Second <bitsecondal@gmail.com>
2019-02-24 16:18:32 -0600

    Updated export database script

---
+++ mysql/config.example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+####################################################################
+# Name: config.example.sh
+# Author: Kenny Robinson, @almostengr
+# Description: Example configuration file
+####################################################################
+
+SQLFILENAME=/tmp/sqlfile.sql
+HOSTNAME="localhost"
+PORT="3306"
+USERNAME="user"
+PASSWORD="pass"
+
