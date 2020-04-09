da0c0964b43928ef775b9a4c547890d834dff9be
Bit Second <bitsecondal@gmail.com>
2020-04-08 22:40:25 -0500

    Added comment about commands

--- utils/aeoffice_monitor_setup.sh
+++ utils/aeoffice_monitor_setup.sh
@@ -11,5 +11,9 @@ OLD_IFS=$IFS
 
 # configure the new mode based on the resolution size
 xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+
+# Type xrandr -q to list your screen outputs
+
 xrandr --addmode VGA-1 1920x1080_60.00
 xrandr --output VGA-1 --mode 1920x1080_60.00
+
