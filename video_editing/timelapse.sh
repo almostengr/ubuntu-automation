1b706c4ad6f30fcc5f09c1f916c84abed57a294e
almostengr <bitsecondal@gmail.com>
2020-04-27 10:29:33 -0500

    Updated timelapse script to only work on MP4 or MOV files

--- video_editing/timelapse.sh
+++ video_editing/timelapse.sh
@@ -2,13 +2,20 @@
 
 echo "Current directory $(pwd)"
 
-for filename in $(ls -1 | grep -v -i timelapse )
+for filename in $(ls -1 *mp4 *MP4 *mov *MOV | grep -v -i timelapse )
 do
 	/bin/date
 
 	echo "Starting on ${filename}"
 
-	/usr/bin/ffmpeg -i ${filename} -vf "setpts=0.75*PTS" -an ${filename}.Timelapse.mp4
+	OUTPUTFILE="${filename}.timelapse.mp4"
+
+	if [[ "${filename}" == *"mov" ]]; then
+		OUTPUTFILE="${filename}.timelapse.mov"
+	fi
+
+	# /usr/bin/ffmpeg -i ${filename} -vf "setpts=0.75*PTS" -an ${filename}.Timelapse.mp4
+	/usr/bin/ffmpeg -i ${filename} -vf "setpts=0.75*PTS" -an ${OUTPUTFILE}
 
 	echo "Done with ${filename}"
 done
