e516bedee1f0fec21b0953f3d8a30f95c0fa79ba
almostengr <bitsecondal@gmail.com>
2020-04-30 19:48:10 -0500

    Report directory space, not entire disk space

--- utils/clean_log_dir.sh
+++ utils/clean_log_dir.sh
@@ -17,7 +17,8 @@ if [ "$(id -u)" == "0" ]; then
 	cd /var/log 
 
 	# display the disk usage before cleanup
-	/bin/df -h .
+	# /bin/df -h .
+	/usr/bin/du -sh /var/log
 
 	# list all the files before removing
 	/usr/bin/find /var/log/* -type f -mtime +${DELAY} -exec ls -la {} \;
@@ -26,7 +27,8 @@ if [ "$(id -u)" == "0" ]; then
 	/usr/bin/find /var/log/* -type f -mtime +${DELAY} -exec rm {} \;
 
 	# display the disk usage after cleanup
-	/bin/df -h .
+	# /bin/df -h .
+	/usr/bin/du -sh /var/log
 else
 	echo "ERROR: Must be root to run script."
 fi
