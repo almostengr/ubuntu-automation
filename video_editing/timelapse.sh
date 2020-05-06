ca65fac92072f9f5e1a5f755612384bf29df8956
Almost Engineer <bitsecondal@gmail.com>
2020-05-05 22:35:52 -0500

    Change to process provided filename

--- video_editing/timelapse.sh
+++ video_editing/timelapse.sh
@@ -1,39 +1,54 @@
 #!/bin/bash
 
-/bin/date
+## DESCRIPTION: Generate a timelapse video from the provided input file.
+## AUTHOR: Kenny Robinson @almostengr
+## DATE: 2020-04-30
+## USAGE: timelapse.sh <filename> 
+## <filename> is the name of the video file that a timelapse will be 
+## generated for.
+
+function log_message {
+    echo "$(/bin/date) ${1}"
+}
+
+log_message "Script starting"
 
 FILENAME="${1}"
 
 # input video file must be provided
 if [[ "${FILENAME}" != "" ]]; then
-	echo "Input video file: ${FILENAME}"
+	log_message "Input video file: ${FILENAME}"
 
-	# for filename in $(ls -1 *mp4 *MP4 *mov *MOV | grep -v -i timelapse )
-	# do
-
-	echo "Starting on ${FILENAME}"
+	log_message "Modifying mp4 filename"
 
 	MODFILENAME=$(echo ${FILENAME} | sed 's/.mp4//g')
 	OUTPUTFILE="${MODFILENAME}.timelapse.mp4"
 
 	if [[ "${FILENAME}" == *"mov" ]]; then
-		# OUTPUTFILE="$(echo ${FILENAME} | sed 's/.mov//g').timelapse.mov"
+		log_message "Modifying .mov filename"
+
 		MODFILENAME=$(echo ${FILENAME} | sed 's/.mov//g')
 		OUTPUTFILE="${MODFILENAME}.timelapse.mov"
-		
+
 	elif [[ "${FILENAME}" == *"mkv" ]]; then
+		log_message "Modifying .mkv filename"
+
 		MODFILENAME=$(echo ${FILENAME} | sed 's/.mkv//g')
 		OUTPUTFILE="${MODFILENAME}.timelapse.mkv"
 	fi
 
-	# /usr/bin/ffmpeg -i ${filename} -vf "setpts=0.75*PTS" -an ${filename}.Timelapse.mp4
-	/usr/bin/ffmpeg -i ${FILENAME} -vf "setpts=0.75*PTS" -an ${OUTPUTFILE}
+	if [ -z "${TIMELAPSESPEED}" ]; then
+		log_message "Timelapse speed not defined"
+		TIMELAPSESPEED=0.5
+	fi
 
-	echo "Done with ${FILENAME}"
-	# done
+	log_message "Generating timelapse video"
+
+	# /usr/bin/ffmpeg -i ${FILENAME} -vf "setpts=0.75*PTS" -an ${OUTPUTFILE}
+	/usr/bin/ffmpeg -i ${FILENAME} -vf "setpts=${TIMELAPSESPEED}*PTS" -an ${OUTPUTFILE}
+
+	log_message "Done generating timelapse video"
 
 else 
-	echo "The video file to timelapse was not provided."
+	log_message "The video file to timelapse was not provided."
 fi
-
-/bin/date
