0f07ac5fd65931029904fb7ef4e3c12c99668245
Bit Second <bitsecondal@gmail.com>
2022-01-17 10:29:21 -0600

    Added backup directory to script

--- utils/clean_log_dir.sh
+++ utils/clean_log_dir.sh
@@ -3,7 +3,8 @@
 ################################################################################
 # AUTHOR: Kenny Robinson, @almostengr
 # DATE CREATED: 2016-05-29
-# DESCRIPTION: Cleans the log directory on the machine. Log and compressed log 
+# DESCRIPTION: Cleans the logs and backup directory on the machine. 
+# Log and compressed log 
 # files that are older than 30 days are automatically discarded. To prevent 
 # permissions error when running the script via cron or other automation, the 
 # run as ID should be a member of the syslog group. 
@@ -24,6 +25,12 @@ if [ "$(id -u)" == "0" ]; then
 
 	# remove the files
 	/usr/bin/find /var/log/* -type f -mtime +${DELAY} -exec rm {} \;
+
+	# list files in backup directory
+	/usr/bin/find /var/backups/* -type f -mtime +${DELAY} -exec ls -la {} \;
+
+	# remove files
+	/usr/bin/find /var/backups/* -type f -mtime +${DELAY} -exec rm {} \;
 
 	# display the disk usage after cleanup
 	# /bin/df -h .
