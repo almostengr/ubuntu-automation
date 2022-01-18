1e5d1160595456ccc50b4e3a8cd222c2e35e03eb
Almost Engineer <bitsecondal@gmail.com>
2022-01-17 22:30:50 -0600

    Updated render script for dash cam videos

--- videos/process_dash_cam_video.sh
+++ videos/process_dash_cam_video.sh
@@ -1,39 +1,57 @@
 #!/bin/bash
 
 ###############################################
-## DESCRIPTION: Script used to create short videos aka YouTube Shorts for the Kenny Ram Dash Cam YouTube channel
+## DESCRIPTION: Script used to create videos for the Kenny Ram Dash Cam YouTube channel
 ## AUTHOR: Kenny Robinson, @almostengr
+## 
+## REFERENCES: 
+## https://superuser.com/questions/939357/how-to-position-drawtext-text
 ###############################################
 
 DASHCAMFOLDER="/mnt/d74511ce-4722-471d-8d27-05013fd521b3/Kenny Ram Dash Cam"
 
+## change to dash cam folder
+
+## beginning of loop
+
+## cd to dash cam folder
+
+## cd into child folder
+
 echo "INFO: $(date) Performing cleanup"
 /bin/rm input.txt details.txt
 
-## todo check if doing short video
+## todo check why intro video causes problems with rendering
+
+# echo "INFO: $(date) Copying video intro"
+# /bin/cp -p "/mnt/d74511ce-4722-471d-8d27-05013fd521b3/ytvideostructure/dash_cam_opening_shortened.mp4" AAAAintro.mp4
 
 echo "INFO: $(date) Making the list of video files"
-for file in $(ls -1 *mp4 *MP4 *MOV *mov) ; 
-do 
+for file in $(ls -1tr *mp4 *MP4 *MOV *mov) ;
+# for file in $(ls -1 *mp4 *MP4 *MOV *mov) ;
+do
 echo "file ${file}" >> input.txt
-echo "$(pwd)" >> details.txt
+echo "$(pwd)/${file}" >> details.txt
 done
 
 BASENAME=$(/usr/bin/basename "$(pwd)")
 OUTPUTNAME="${BASENAME}$(/bin/date +%Y%m%d%H%M)"
+VIDEOTITLE=$(cut -d ";" -f 1 <<< "${BASENAME}")
+
+echo "INFO: Video Title: ${VIDEOTITLE}"
 
 echo "INFO: $(date) Rendering video"
 
-## todo Check if doing 720 or 1080 video
-
-## no audio; 920 for 720p, 1420 for 1080p
-/usr/bin/ffmpeg -f concat -i input.txt -an -vf "drawtext=textfile:Kenny Ram Dash Cam:fontcolor=white:fontsize=30:x=920:y=20:box=1:boxborderw=7:boxcolor=black@0.7" "${OUTPUTNAME}.mp4"
+# /usr/bin/ffmpeg -f concat -i input.txt -an -vf "drawtext=textfile:Kenny Ram Dash Cam:fontcolor=white:fontsize=30:x=920:y=20:box=1:boxborderw=7:boxcolor=black@0.7" "${OUTPUTNAME}.mp4"
+/usr/bin/ffmpeg -y -f concat -i input.txt -an -vf "drawtext=textfile:Kenny Ram Dash Cam:fontcolor=white:fontsize=25:x=w-tw-50:y=50:box=1:boxborderw=7:boxcolor=black@0.3, drawtext=textfile:'${VIDEOTITLE}':fontcolor=white:fontsize=25:x=50:y=50:box=1:boxborderw=7:boxcolor=black@0.3" "${OUTPUTNAME}.mp4"
 
 echo "INFO: $(date) Packaging video into archive"
-tar -czvf "${OUTPUTNAME}.tar.gz" "${OUTPUTNAME}.mp4" details.txt
+tar -czvf "${BASENAME}.tar.gz" "${OUTPUTNAME}.mp4" details.txt
 
 echo "INFO: $(date) Moving video to upload directory"
 /bin/mv "${OUTPUTNAME}.mp4" "${DASHCAMFOLDER}/upload"
 
 echo "INFO: $(date) Moving archive to archive directory"
-/bin/mv "${OUTPUTNAME}.tar.gz" "${DASHCAMFOLDER}/archive"
+/bin/mv "${BASENAME}.tar.gz" "${DASHCAMFOLDER}/archive"
+
+## end of loop
