b8e08aee536eaaed4312766a0f826868972e0728
Almost Engineer <bitsecondal@gmail.com>
2022-01-19 22:21:08 -0600

    Removed large overlay at beginning of video

--- videos/process_dash_cam_video.sh
+++ videos/process_dash_cam_video.sh
@@ -112,15 +112,15 @@ CHANNELNAME2=", ${CHANNELNAME}@${DIMMEDBG}:enable='gt(t,20)'"
 ## video title
 TITLETEXT=$(${VIDEOTITLE} | fold -sw 60)
 TITLE=", drawtext=textfile=title.txt:fontcolor=${COLOR}:box=1:boxborderw=7:boxcolor=black"
-TITLE1="${TITLE}:enable='between(t,0,5)':fontsize=${FONTSIZE}+15:${CENTERED}"
-TITLE3="${TITLE}:fontsize=${FONTSIZE}:${UPPERLEFT}:enable='between(t,5,20)'"
+# TITLE1="${TITLE}:enable='between(t,0,5)':fontsize=${FONTSIZE}+15:${CENTERED}"
+TITLE3="${TITLE}:fontsize=${FONTSIZE}:${UPPERLEFT}:enable='between(t,0,20)'"
 TITLE2="${TITLE}@${DIMMEDBG}:fontsize=${FONTSIZE}:${UPPERLEFT}:enable='gt(t,20)'"
 
 # /usr/bin/ffmpeg -f concat -i input.txt -an -vf "drawtext=textfile:Kenny Ram Dash Cam:fontcolor=${COLOR}:fontsize=30:x=920:y=20:box=1:boxborderw=7:boxcolor=black@0.7" "${OUTPUTNAME}.mp4"
-/usr/bin/ffmpeg -y -f concat -i input.txt -an -vf "${CHANNELNAME1}${CHANNELNAME2}${TITLE1}${TITLE2}${TITLE3}${DESTINATIONDETAILS}${MAJORROADDETAILS}" "${OUTPUTNAME}.mp4"
+/usr/bin/ffmpeg -y -f concat -i input.txt -an -vf "${CHANNELNAME1}${CHANNELNAME2}${TITLE2}${TITLE3}${DESTINATIONDETAILS}${MAJORROADDETAILS}" "${OUTPUTNAME}.mp4"
 
 echo "INFO: $(date) Packaging video into archive"
-/bin/tar -czvf "${BASENAME}.tar.gz" "${OUTPUTNAME}.mp4" *.txt *.png
+/bin/tar -czvf "${BASENAME}.tar.gz" "${OUTPUTNAME}.mp4" majorroads.txt destination.txt details.txt *.png
 
 echo "INFO: $(date) Moving video and thumbnail to upload directory"
 /bin/mv "${OUTPUTNAME}.mp4" "${DASHCAMFOLDER}/upload"
