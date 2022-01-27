60d9ee6f416cd501acb63221a75b554a8eb690cd
Almost Engineer <bitsecondal@gmail.com>
2022-01-26 22:23:27 -0600

    Updated ignore directories

--- videos/process_dash_cam_video.sh
+++ videos/process_dash_cam_video.sh
@@ -142,7 +142,7 @@ do
 
     checkDiskSpace
 
-    if [[ "${VIDDIRECTORY}" == "upload/" || "${VIDDIRECTORY}" == "archive/" || "${VIDDIRECTORY}" == "*ignore*" ]]; then
+    if [[ "${VIDDIRECTORY}" == "upload/" || "${VIDDIRECTORY}" == "archive/" || "${VIDDIRECTORY}" == *"ignore"* ]]; then
         continue
     fi
 
